6bd3dffb68e71175c5129eede5edca1d662ad230
xiaoxiae <tomas@slama.dev>
2021-04-27 16:59:46 +0200

    comments and fade to utilitites

--- utilitites.py
+++ utilitites.py
@@ -2,12 +2,14 @@
 from manim import *
 from math import *
 from random import *
+import networkx as nx
 
 def create_code(self, code):
-    """Create code more evenly."""
+    """Creates code more evenly."""
     return [Create(code.background_mobject), Write(code.code), Write(code.line_numbers)]
 
 def myCode(*args, **kwargs):
+    """Declares a nice-looking code block."""
     code = Code(*args, **kwargs, font="Fira Mono", line_spacing=0.35, style="Monokai")
     code.background_mobject[0].set_style(fill_opacity=0)
     return code
@@ -17,6 +19,19 @@ def create_output(self, output):
     return [Create(output.background_mobject), Write(output.code)]
 
 def myOutput(*args, **kwargs):
+    """Declares a nice-looking output block."""
     code = Code(*args, **kwargs, font="Fira Mono", line_spacing=0.35, style="Monokai", insert_line_no=False)
     code.background_mobject[0].set_style(fill_opacity=0)
     return code
+
+def highlightText(text):
+    for i in range(1, len(text), 2):
+        text[i].set_color(YELLOW)
+
+def fade(f):
+    """A decorator for construct method of scenes where all objects should fade at the end."""
+    def inner(self):
+        f(self)
+        self.play(*map(FadeOut, self.mobjects))
+
+    return inner
