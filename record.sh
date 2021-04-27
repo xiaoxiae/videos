887109d15e28234efb4fd08e39e3512c333ac082
xiaoxiae <tomas@slama.dev>
2021-04-27 17:00:23 +0200

    record script and noise profile

--- record.sh
+++ record.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # a simple script for recording a sentence and playing it back
-arecord -vv -fdat $1.mp4
+arecord -vv -fdat $1.wav
+sox $1.wav a.wav noisered record.noise-profile 0.3
+rm $1.wav
+mv a.wav $1.wav
