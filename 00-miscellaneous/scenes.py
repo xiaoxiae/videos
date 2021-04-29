bc258086d9b62c7a4deb573edfd821afd8555e63
xiaoxiae <tomas@slama.dev>
2021-04-29 14:25:51 +0200

    move intro and outro things to miscellaneous

--- 00-miscellaneous/scenes.py
+++ 00-miscellaneous/scenes.py
@@ -19,4 +19,15 @@ class Logo(Scene):
             run_time = 1
         )
 
+class LogoBottomCorner(Scene):
+    def construct(self):
+        t = SVGMobject("logo-t.svg").scale(0.7).set_color_by_gradient((WHITE, WHITE, GRAY, GRAY))
+        s = SVGMobject("logo-s.svg").scale(0.7)
 
+        s.shift(DOWN * 2.8 + RIGHT * 6)
+        t.shift(DOWN * 2.8 + RIGHT * 6)
+
+        self.play(
+            FadeIn(t),
+            FadeIn(s),
+        )
