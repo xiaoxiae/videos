fff94cccf2808735446748904b0520e58fbb793c
xiaoxiae <tomas@slama.dev>
2021-05-11 21:04:42 +0200

    fixed record script

--- record.sh
+++ record.sh
@@ -3,9 +3,7 @@
 # a simple script for recording a sentence and playing it back
 arecord -vv -fdat $1.wav
 sox $1.wav a.wav noisered record.noise-profile 0.3
-sox a.wav b.wav trim 0 -0.5
-sox b.wav c.wav trim 0 =1.5
+sox a.wav b.wav trim 0.5 -0.5
 rm $1.wav
 rm a.wav
-rm b.wav
-mv c.wav $1.wav
+mv b.wav $1.wav
