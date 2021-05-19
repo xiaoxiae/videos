3fd0a9c1da09b3a89b47b64384902e94d1454258
xiaoxiae <tomas@slama.dev>
2021-05-19 13:50:16 +0200

    update build.py (closes #2, closes #3)

--- build.py
+++ build.py
@@ -5,12 +5,40 @@ import os
 
 video_directory = "video"
 
+import argparse
+import datetime
+import os
+
+
+parser = argparse.ArgumentParser(
+    description="A script for building files for the video."
+)
+
+parser.add_argument(
+    "-s", "--scene",
+    help="the name of the scene to build; builds all when omitted",
+    default=None,
+)
+
+parser.add_argument(
+    "-q", "--quality",
+    choices=["l", "m", "h", "k"],
+    default="h",
+    help="the quality of the video"
+)
+
+# licenses (to give as options)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# get the parser and parse the commands
+arguments = parser.parse_args()
+
 # from scratch!
 if os.path.exists(video_directory):
     shutil.rmtree(video_directory)
 
 # call Manim
-subprocess.Popen(["manim", "scenes.py", "-aqk"]).communicate()
+subprocess.Popen(["manim", "scenes.py", "-aqh"] + ([] if arguments.scene is None else [arguments.scene])).communicate()
 
 # rename to [1..n].mp4
 for scene in os.listdir(video_directory):
@@ -18,7 +46,7 @@ for scene in os.listdir(video_directory):
         with open(os.path.join(video_directory, scene, "partial_movie_file_list.txt")) as f:
             for i, video in enumerate(f.read().splitlines()[1:]):
                 path, name = os.path.split(video[11:-1])
-                
+
                 original_path = os.path.join(path, name)
                 changed_path = os.path.join(path, f"{i+1}.mp4")
                 image_path = os.path.join(path, f"{i+1}.png")
