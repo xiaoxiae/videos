d53c6d852c9f3f7ec801efe6d85b13ca2146d91c
xiaoxiae <tomas@slama.dev>
2021-08-11 12:00:30 +0200

    Edmonds: intro, core

--- 06-edmonds-blossom/SCRIPT.md
+++ 06-edmonds-blossom/SCRIPT.md
@@ -2,6 +2,7 @@
 INTRODUCTION
 ---
 
+# Intro
 TODO: motivation, this sucks. Imagine you have a group of people,each having preferences regarding who they'd like to be matched with. Your task is to create as many pairs as possible while respecting the preferences (i. e. no two people will be matched).
 
 You certainly could test all possible options, but this would, for larger groups of people, take quite a while. We need something better.
@@ -14,19 +15,25 @@ Formally, a matching in a graph is a subset of edges, such that no two share a v
 
 We'll call vertices that are not in the matching exposed vertices.
 
+# Core
 The core idea behind the algorithm are „augmenting paths.“ An augmenting path in a graph with some matching is an alternating sequence of edges in the matching and edges not in the matching, where the first and the last vertex is exposed.
 
-TODO: animace tohohle na nějakém grafu
+On the graph, one such augmenting path is highlighted in red.
 
-As the name suggests, augmenting paths can improve (or augment) the size of the current matching by 1 by switching the edges on the path. Also, if a graph **doesn't** contain an augmenting path, then the matching is maximal.
+As the name suggests, augmenting paths can improve (or augment) the size of the current matching by switching the edges on the path.
 
-Combining these two facts gives us the algorithm -- repeatedly look for augmenting paths and use them to improve the matching, until there are no more, at which point we know that the matching is maximal.
+Is essentially the algorithm -- repeatedly look for augmenting paths and use them to improve the matching.
+
+One thing to note is that if a graph **doesn't** contain an augmenting path, then the matching **is** maximal. This ensures that we're done when we run out of augmenting paths.
 
 ---
 TREE PERFECT MATCHING
 ---
 
+# Tree
 First, let's think about how to find augmenting paths in a tree (a graph without cycles). This will be pretty straight-forward -- run a BFS from some exposed vertex until we find another one.
+
+Initially, all are exposed, since the pairing is empty.
 
 TODO: animace
 
--- 06-edmonds-blossom/scenes.py
+++ 06-edmonds-blossom/scenes.py
@@ -5,6 +5,52 @@ def visualizeBipariteBlossom(graph, pairing=[]):
     """Visualize the blossom algorithm for a bipartite graph."""
 
 
+PAIRING_COLOR = YELLOW
+EXPOSED_COLOR = ORANGE
+AUGMENTING_COLOR = RED
+
+MATCHED_WIDTH = 15
+
+def match_edge(mob, change_color=True):
+    mob.set_stroke_width(MATCHED_WIDTH)
+
+    if change_color:
+        mob.set_color(PAIRING_COLOR)
+
+    return mob
+
+def unmatch_edge(mob, change_color=True):
+    mob.set_stroke_width(4)
+
+    if change_color:
+        mob.set_color(WHITE)
+
+    return mob
+
+
+def augment_path(self, g, path):
+    A = path
+    AV = edgesToVertices(A)
+
+    self.play(
+        *[g.edges[e].animate.set_color(AUGMENTING_COLOR) for e in A],
+        *[g.vertices[v].animate.set_color(AUGMENTING_COLOR) for v, _ in A],
+        *[g.vertices[v].animate.set_color(AUGMENTING_COLOR) for _, v in A],
+    )
+
+    self.play(
+            *[ApplyFunction(lambda x: match_edge(x, change_color=False),g.edges[e]) for e in A[::2]],
+            *[ApplyFunction(lambda x: unmatch_edge(x, change_color=False),g.edges[e]) for e in A[1::2]],
+    )
+
+    self.play(
+        *[g.edges[e].animate.set_color(PAIRING_COLOR) for e in A[::2]],
+        *[g.edges[e].animate.set_color(WHITE) for e in A[1::2]],
+        *[g.vertices[v].animate.set_color(PAIRING_COLOR) for v, _ in A[::2]],
+        *[g.vertices[v].animate.set_color(PAIRING_COLOR) for _, v in A[::2]],
+    )
+
+
 class Intro(Scene):
     def construct(self):
         g = parse_graph("""
@@ -23,40 +69,96 @@ class Intro(Scene):
 5 12 <5.320620964273308, -6.074692063436867> <11.495122080884329, -4.388454491563957>
 3 11 <0.8751494485961517, 2.8894436903527834> <0.4357598447196547, -3.4516057795938906>
 2 14 <10.542340602049554, 5.0486231945767255> <16.235759844719652, 1.2483942204061114>
-                """, s=0.13, t=0.13).scale(1.2)
+                """, s=0.13, t=0.13).scale(1.3)
 
         self.play(Write(g))
 
-        self.play(*[Indicate(g.vertices[v], scale_factor=1) for v in g.vertices])
-        self.play(*[Indicate(g.edges[v], scale_factor=1) for v in g.edges])
-
-        M = [(1, 2), (3, 6), (8, 13), (5, 12)]
+        M = [(1, 2), (8, 13), (5, 12)]
         MV = edgesToVertices(M)
 
-        def apply_function(mob):
-            mob.set_stroke_width(9)
-            mob.set_color(YELLOW)
-            return mob
-
-        def unapply_function(mob):
-            mob.set_stroke_width(4)
-            mob.set_color(WHITE)
-            return mob
-
         self.play(
-            *[ApplyFunction(apply_function,g.edges[e]) for e in M],
-            *[g.vertices[v].animate.set_color(YELLOW) for v, _ in M],
-            *[g.vertices[v].animate.set_color(YELLOW) for _, v in M],
+            *[ApplyFunction(match_edge,g.edges[e]) for e in M],
+            *[g.vertices[v].animate.set_color(PAIRING_COLOR) for v, _ in M],
+            *[g.vertices[v].animate.set_color(PAIRING_COLOR) for _, v in M],
         )
 
         MP = getMaximalPairing(list(g.edges))
         MPV = edgesToVertices(MP)
 
         self.play(
-            *[ApplyFunction(apply_function,g.edges[e]) for e in MP if e not in M],
-            *[g.vertices[v].animate.set_color(YELLOW) for v, _ in MP if v not in MV],
-            *[g.vertices[v].animate.set_color(YELLOW) for _, v in MP if v not in MV],
-            *[ApplyFunction(unapply_function,g.edges[e]) for e in M if e not in MP],
+            *[ApplyFunction(match_edge,g.edges[e]) for e in MP if e not in M],
+            *[g.vertices[v].animate.set_color(PAIRING_COLOR) for v, _ in MP if v not in MV],
+            *[g.vertices[v].animate.set_color(PAIRING_COLOR) for _, v in MP if v not in MV],
+            *[ApplyFunction(unmatch_edge,g.edges[e]) for e in M if e not in MP],
             *[g.vertices[v].animate.set_color(WHITE) for v, _ in M if v not in MPV],
             *[g.vertices[v].animate.set_color(WHITE) for _, v in M if v not in MPV],
+        )
+
+        self.play(
+            *[Circumscribe(g.vertices[v], Circle, color=EXPOSED_COLOR) for v in g.vertices if v not in MPV],
+            *[g.vertices[v].animate.set_color(EXPOSED_COLOR) for v in g.vertices if v not in MPV],
+        )
+
+
+class Core(Scene):
+    def construct(self):
+        g = parse_graph("""
+1 2 <10.402934501867037, 17.34415069651897> <16.203839106744862, 19.547491168818176>
+2 3 <16.203839106744862, 19.547491168818176> <22.178420402084758, 17.787256748775246> 
+3 4 <22.178420402084758, 17.787256748775246> <20.774348346247653, 23.755044257314765>
+2 4 <16.203839106744862, 19.547491168818176> <20.774348346247653, 23.755044257314765>
+4 5 <20.774348346247653, 23.755044257314765> <27.05817492976113, 23.859707295924565>
+6 5 <28.449708119653423, 17.768804208250373> <27.05817492976113, 23.859707295924565>
+4 7 <20.774348346247653, 23.755044257314765> <17.47242508549339, 29.021438342487553>
+5 8 <27.05817492976113, 23.859707295924565> <31.15502907598739, 28.586235859209886>
+8 9 <31.15502907598739, 28.586235859209886> <37.31660856822276, 28.245054265211092> 
+6 10 <28.449708119653423, 17.768804208250373> <34.660938508332286, 16.631467003826533>
+10 11 <34.660938508332286, 16.631467003826533> <40.60957109101587, 18.40703837992414>
+3 6 <22.178420402084758, 17.787256748775246> <28.449708119653423, 17.768804208250373>
+5 12 <27.05817492976113, 23.859707295924565> <33.0788344527837, 22.821541250821294>
+2 13 <16.203839106744862, 19.547491168818176> <12.52911329849324, 24.494238155627645>
+                """, s=0.13, t=0.13).scale(1.3)
+
+        self.play(Write(g))
+
+        M = [(5, 8), (6, 10), (2, 4)]
+        MV = edgesToVertices(M)
+
+        self.play(
+            *[ApplyFunction(match_edge,g.edges[e]) for e in M],
+            *[g.vertices[v].animate.set_color(PAIRING_COLOR) for v, _ in M],
+            *[g.vertices[v].animate.set_color(PAIRING_COLOR) for _, v in M],
+            *[g.vertices[v].animate.set_color(EXPOSED_COLOR) for v in g.vertices if v not in MV],
+        )
+
+        augment_path(self, g, [(8, 9), (5, 8), (4, 5), (2, 4), (1, 2)])
+        augment_path(self, g, [(10, 11), (6, 10), (3, 6)])
+        augment_path(self, g, [(5, 12), (4, 5), (4, 7)])
+
+
+class Tree(Scene):
+    def construct(self):
+        g = parse_graph("""
+1 2 <22.112572766007876, -2.5264304107833246> <25.67796210932871, 2.697892851581728>
+3 1 <28.55611115849213, -3.7428115450105133> <22.112572766007876, -2.5264304107833246>
+4 1 <15.428111442950147, -2.829960372608953> <22.112572766007876, -2.5264304107833246>
+4 5 <15.428111442950147, -2.829960372608953> <8.976964571929539, -3.0504356092202367>
+3 6 <28.55611115849213, -3.7428115450105133> <34.04768084651785, -0.5973824149390512>
+3 7 <28.55611115849213, -3.7428115450105133> <32.64776811333644, -8.460442932301625>
+5 8 <8.976964571929539, -3.0504356092202367> <3.834695753774831, -6.50435350211315>
+4 9 <15.428111442950147, -2.829960372608953> <14.226722555124457, -8.942190801950318>
+2 10 <25.67796210932871, 2.697892851581728> <28.496351384614382, 8.224513316804416>
+1 11 <22.112572766007876, -2.5264304107833246> <22.95614485199056, -8.724125830834108>
+1 12 <22.112572766007876, -2.5264304107833246> <19.316663589836477, 3.048818090535028>
+13 4 <11.896071360485621, 2.430059482684668> <15.428111442950147, -2.829960372608953>
+14 6 <38.90070190924257, 3.2512436388123502> <34.04768084651785, -0.5973824149390512>
+13 15 <11.896071360485621, 2.430059482684668> <8.433034228381587, 7.599704780509387>
+5 16 <8.976964571929539, -3.0504356092202367> <3.6702917967931405, 0.23588429272754086>
+17 12 <17.000076691912383, 8.77194474648056> <19.316663589836477, 3.048818090535028>
+                """, s=0.11, t=0.11).scale(1.3)
+
+        self.play(Write(g))
+
+        self.play(
+            *[g.vertices[v].animate.set_color(EXPOSED_COLOR) for v in g.vertices],
         )
