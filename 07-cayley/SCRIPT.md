f6d14521998dfa1731feea6ddafc1c1614ad4c05
xiaoxiae <tomas@slama.dev>
2021-09-04 00:00:43 +0200

    Cayley: finish proof animation

--- 07-cayley/SCRIPT.md
+++ 07-cayley/SCRIPT.md
@@ -6,7 +6,7 @@ INTRODUCTION
 In this short video, we'll be looking at an elegant proof of Cayley's formula about the number of spanning trees in a complete graph. It was discovered by Jim Pitman, and is a great example of a powerful proof technique called double counting.
 
 # Definitions
-A complete graph on $n$ vertices is a graph, where each pair of vertices is connected by an edge.
+A complete graph on $n$ vertices (denoted $K_n$) is a graph, where each pair of vertices is connected by an edge.
 
 A spanning tree in a graph is a subgraph that is a tree and includes all vertices of the original graph. A good way to think of it is that it's the smallest subgraph that keeps the graph connected.
 
@@ -17,7 +17,7 @@ PROBLEM STATEMENT
 # Formula
 Cayley's formula is rather simple: it states, that the number of spanning trees of a complete graph on $n$ vertices is $\kappa(n) = n^{n-2}$. While simple in statement, it's definitely not trivial to prove.
 
-Also, it's worth noting that these three spanning trees differ. Although they are the same in structure (the fancy term here is isomorphic), they differ in where the edges actually are, which is what matters.
+Also, it's worth noting that these three spanning trees are not equal. Although they are the same in structure (the fancy term here is isomorphic), they differ in where the edges actually are, which all that matters.
 
 ---
 PROOF
@@ -25,23 +25,28 @@ PROOF
 
 As I've mentioned earlier, the proof technique that we'll be using is double counting. This involves, as the name suggests, counting some quantity in two different ways.
 
-For this proof, we'll be counting the number of oriented trees on $n$ vertices that have a root and some numbering of edges. Let's call this quantity $\tau$.
+For this proof, we'll be counting the number of oriented trees on $n$ vertices that have a root and some numbering of edges. Let's call this quantity $\tau(n)$.
 
-Here are two examples of what the trees could look like.
+Here are two examples of what the trees could look like. As you can see, the edges are oriented such that they point towards the root.
 
-The first way to count will be using Cayley's formula, which by itself equals the number of trees on $n$ vertices. Additionally, there are $n$ ways to select the root and $(n-1)!$ ways to select the edge numbering, bringing the total to $\kappa(n) \cdot n \cdot (n-1)!$.
+The first way to count will be straightforward: to begin with, let's count the number of trees without root and edge numbering -- this, from definition, equals $\kappa(n)$. Next, the root can be any of the $n$ vertices. Finally, the edge numbers can be any permutation of numbers from $0$ to $n-1$ (which is the number of edges), bringing the total to $\kappa(n) \cdot n \cdot (n-1)!$.
 
-The other way will be constructive. Imagine we're building the tree by adding oriented edges between the vertices.
+The other way will be constructive. Imagine we're building the tree by adding oriented edges between the vertices. Taking $n = 7$ as an example, let's add a few vertices.
 
-Let's say we've already added $k$ of them, so the graph looks like this. It is made up of components that all have individual roots.
+Looking at the graph, we can see it is made up of smaller components, each having a single root.
 
-Looking at the graph, we can make two observations about the edge we're about to add:
+We can now make two observations about the edge we're about to add:
 
-1. it has to lead from the root of a component, because otherwise the edges wouldn't point towards the root
-2. it can't be between two vertices of a single component, as it would create a cycle
+1. it has to start in the root of a component, because otherwise some edges wouldn't point towards the root
+2. it can't be between two vertices of a single component, as the component wouldn't be a tree anymore
 
-Putting this together, let's count the number of ways we can add an edge at each step.
+Putting this together, let's count the number of ways we can add the $k$-th edge.
 
-The edge can end in any vertex, of which there are $n$. When it comes to start, we know it has to start from root of some component as to not break the orientation (observation 1). Additionally, it can't start in the vertex where it ends, so from the $n - k$ components, one is reserved, bringing the total to $n - k - 1$.
+It can end in any vertex, of which there are $n$. When it comes to where it can start, we know it has to start from the root of some component (observation 1). and it can't start in the component where it ends (observation 2), so from the $n - k$ components, it can only start in $n - k - 1$ of them.
 
-To build the entire tree, $n - 1$ arrows must be added, resulting in the following formula.
+To build the entire tree, $n - 1$ edges must be added. At the $k$-th step, there are $n \cdot (n - k - 1)$ ways to add it, resulting in the following formula.
+
+Taking the $n$ out, it becomes $n^{n - 1}$. As for the product, notice that it's just $(n-1)!$.
+
+Using the previous way to count, we can simplify, divide by $n$ and we're done.
+
--- 07-cayley/scenes.py
+++ 07-cayley/scenes.py
@@ -128,11 +128,27 @@ class Formula(Scene):
             Write(ne2),
         )
 
+
+def create_oriented_edge(g, v, w):
+    """Create an oriented edge from v to w."""
+    v_center = g.vertices[v].get_center()
+    w_center = g.vertices[w].get_center()
+
+    center = (v_center + w_center) / 2
+    delta = (v_center - w_center) / 2
+
+    buffer = 0.23
+    mag = (delta[0] ** 2 + delta[1] ** 2)**(1/2)
+    c = (mag - buffer) / mag
+
+    return Arrow(center + delta * c, center - delta * c, max_tip_length_to_length_ratio=30, buff=0)
+
+
 def orient_from_root(g, root):
     queue = [root]
     explored = set([root])
+    e = []
 
-    buffer = 0.23
     g.clear_updaters()
 
     g.vertices[root].set_color(HIGHLIGHT_COLOR)
@@ -153,20 +169,36 @@ def orient_from_root(g, root):
 
             w, v = current, v
 
-            v_center = g.vertices[v].get_center()
-            w_center = g.vertices[w].get_center()
-
-            center = (v_center + w_center) / 2
-            delta = (v_center - w_center) / 2
-
-            mag = (delta[0] ** 2 + delta[1] ** 2)**(1/2)
-            c = (mag - buffer) / mag
-
-            a = Arrow(center + delta * c, center - delta * c, max_tip_length_to_length_ratio=30, buff=0)
+            e.append((v, w))
+            a = create_oriented_edge(g, v, w)
 
             g.edges[(v, w) if (v, w) in g.edges else (w, v)].become(a)
 
-def add_edge_numbering(graph, scale=0.8, shift_coefficient=0.3, numbers=None, return_only=False):
+    return e
+
+
+def create_edge_number(g, v, w, i, scale=0.8, shift_coefficient=0.3, side=False):
+    number = Tex(str(i)).scale(scale)
+
+    v_center = g.vertices[v].get_center()
+    w_center = g.vertices[w].get_center()
+
+    center = (v_center + w_center) / 2
+    delta = (v_center - w_center)
+    delta /= (delta[0] ** 2 + delta[1] ** 2)**(1/2)
+
+    # rotate 90 degrees
+    if side:
+        delta[0], delta[1] = -delta[1], delta[0]
+    else:
+        delta[0], delta[1] = delta[1], -delta[0]
+
+    number.move_to(center).shift(delta * shift_coefficient)
+
+    return number
+
+
+def add_edge_numbering(graph, numbers=None, return_only=False):
     if not numbers:
         numbers = [i + 1 for i in range(len(graph.edges))]
         shuffle(numbers)
@@ -177,19 +209,7 @@ def add_edge_numbering(graph, scale=0.8, shift_coefficient=0.3, numbers=None, re
         numbers_tex = []
 
     for i, (v, w) in enumerate(graph.edges):
-        number = Tex(str(numbers[i])).scale(scale)
-
-        v_center = graph.vertices[v].get_center()
-        w_center = graph.vertices[w].get_center()
-
-        center = (v_center + w_center) / 2
-        delta = (v_center - w_center)
-        delta /= (delta[0] ** 2 + delta[1] ** 2)**(1/2)
-
-        # rotate 90 degrees
-        delta[0], delta[1] = delta[1], -delta[0]
-
-        number.move_to(center).shift(delta * shift_coefficient)
+        number = create_edge_number(graph, v, w, numbers[i])
 
         if not return_only:
             graph.add(number)
@@ -248,8 +268,8 @@ class Proof(Scene):
 
         self.play(FadeOut(g1), FadeOut(g2))
 
-        OFFSET = RIGHT * 2.9
-        OFFSET2 = LEFT * 5.3
+        OFFSET = LEFT * 2.9
+        OFFSET2 = RIGHT
 
         text_1 = Tex(r"$\tau(n)$").shift(DOWN * DOWN_C + OFFSET2)
         text_2 = Tex(r"$= \kappa(n)$").next_to(text_1, RIGHT)
@@ -304,20 +324,34 @@ class Proof(Scene):
         self.play(FadeOut(g_under))
 
         g_prev = None
+        prev_edges = []
         for root in range(n):
             g_curr = Graph([i for i in range(n)],
                     [(i, j) for i in range(n) for j in range(n) if i < j],
                     layout="circular", layout_scale=0.6).scale(GRAPH_SCALE).shift(DOWN * DOWN_C + OFFSET)
 
             strip_graph(g_curr, edges)
-            orient_from_root(g_curr, root)
+
+            eee = orient_from_root(g_curr, root)
+
+            def unfuk(v, w, g):
+                return g.edges[(v, w) if (v, w) in g.edges else (w, v)]
+
+            self.remove(g_prev)
+            self.add(g_curr)
 
             self.play(
-                FadeIn(g_curr),
-                *([] if g_prev is None else [FadeOut(g_prev)]),
+                *[FadeIn(unfuk(*e, g_curr)) for e in eee if e not in prev_edges],
+                *[FadeOut(unfuk(*e, g_prev)) for e in prev_edges if e not in eee],
+                *([] if g_prev is None else [g_prev.vertices[root - 1].animate.set_color(WHITE)]),
+                g_curr.vertices[root].animate.set_color(HIGHLIGHT_COLOR),
                 run_time=0.7,
             )
 
+            if g_prev is not None:
+                self.remove(g_prev.vertices[root - 1])
+
+            prev_edges = eee
             g_prev = g_curr
 
         prev_numbers = add_edge_numbering(g_curr, return_only=True)
@@ -331,7 +365,7 @@ class Proof(Scene):
             waits = start + [0.004] * count * 2
 
             numbers = add_edge_numbering(g_curr, numbers=p, return_only=True)
-            #self.play(*[Transform(a, b) for a, b in zip(prev_numbers, numbers)], run_time=waits[i])
+            self.play(*[Transform(a, b) for a, b in zip(prev_numbers, numbers)], run_time=waits[i])
 
         self.play(Write(text_4), Write(text_4d))
 
@@ -340,10 +374,272 @@ class Proof(Scene):
             *[FadeOut(n) for n in prev_numbers],
             FadeOut(g_curr),
             FadeOut(g3),
-            text_1.animate.shift(delta),
-            text_2.animate.shift(delta),
-            text_3.animate.shift(delta),
-            text_3d.animate.shift(delta),
-            text_4.animate.shift(delta),
-            text_4d.animate.shift(delta),
+            FadeOut(text_1),
+            FadeOut(text_2),
+            FadeOut(text_3),
+            FadeOut(text_3d),
+            FadeOut(text_4),
+            FadeOut(text_4d),
         )
+
+        n = 7
+        g4 = Graph([i for i in range(n)],
+                [],
+                layout="circular", layout_scale=0.6).scale(GRAPH_SCALE).shift(DOWN * DOWN_C)
+
+        for v in g4.vertices:
+            g4.vertices[v].set_color(HIGHLIGHT_COLOR)
+
+        self.play(Write(g4))
+
+        edges = []
+        numbers = []
+
+        operation_list = [
+            (0, 1, True),
+            (4, 3, False),
+            (3, 5, False),
+        ]
+
+        def _add_edge(v, w, s, i=[0]):
+            edges.append(create_oriented_edge(g4, v, w))
+            numbers.append(create_edge_number(g4, v, w, i[-1], side=s))
+
+            i[-1] += 1
+            self.play(
+                    Write(edges[-1]),
+                    Write(numbers[-1]),
+                    g4.vertices[v].animate.set_color(WHITE),
+                    )
+
+        _add_edge(*operation_list[0])
+        _add_edge(*operation_list[1])
+        _add_edge(*operation_list[2])
+
+        OFFSET += LEFT * 0.7
+        self.play(
+                g4.animate.shift(OFFSET),
+                *[n.animate.shift(OFFSET) for n in numbers],
+                *[e.animate.shift(OFFSET) for e in edges],
+                )
+
+        parts = [
+            r"\small $1.\ $start in component's root\\ " + "\n",
+            r"\small $2.\ $can't be in a single component\\ "
+        ]
+
+        l = Tex(*parts)
+        l.arrange(DOWN, aligned_edge=LEFT).shift(RIGHT * 3.0 + DOWN * 1)
+
+        self.play(Write(l[0]))
+
+        edge = create_oriented_edge(g4, 3, 1)
+        edge.set_color(RED)
+
+        self.bring_to_back(edge)
+        self.play(Write(edge))
+        self.play(FadeOut(edge))
+
+        self.play(Write(l[1]))
+
+        edge = create_oriented_edge(g4, 5, 4)
+        edge.set_color(RED)
+
+        self.bring_to_back(edge)
+        self.play(
+                Write(edge),
+                g4.vertices[5].animate.set_color(WHITE),
+                )
+        self.play(
+                FadeOut(edge),
+                g4.vertices[5].animate.set_color(HIGHLIGHT_COLOR),
+                )
+
+        self.play(
+                l[0].animate.shift(UP * 1.6),
+                l[1].animate.shift(UP * 1.6),
+                )
+
+
+        parts2 = [
+            r"\small end:\\ " + "\n",
+            r"\small start:\\ ",
+        ]
+
+        parts3 = [
+            r"\small $n$\\ " + "\n",
+            r"\small $n - k - 1$\\ ",
+        ]
+
+
+        l2 = Tex(*parts2)
+        l2.arrange(DOWN, aligned_edge=RIGHT).shift(RIGHT * 1.8 + DOWN * 1.7)
+
+        l3 = Tex(*parts3)
+        l3.arrange(DOWN, aligned_edge=LEFT).shift(RIGHT * 3.5 + DOWN * 1.7)
+        l3[0].set_color(YELLOW)
+        l3[1].set_color(YELLOW)
+
+        l3[0].shift(DOWN * 0.07)
+        l3[1].shift(DOWN * 0.07)
+
+        self.play(Write(l2[0]))
+        self.play(Write(l3[0]))
+        self.play(Write(l2[1]))
+        self.play(Write(l3[1]))
+
+        self.play(FadeOut(l2))
+
+        lp = Tex(r"\small$($")
+        rp = Tex(r"\small$)$")
+        cd = Tex(r"\small$\cdot$")
+
+
+        l3[0].shift(LEFT * 0.75 + DOWN * 0.3)
+
+        delta = (l3[0].get_center() - l3[1][0].get_center())[1] * UP
+
+        l3[1].shift(delta)
+
+        lp.next_to(l3[1], LEFT).shift(RIGHT * 0.2)
+        rp.next_to(l3[1], RIGHT).shift(LEFT * 0.2)
+        cd.next_to(l3[0], RIGHT).shift(LEFT * 0.1)
+
+        l3[1].shift(-delta)
+        l3[0].shift(RIGHT * 0.75 + UP * 0.3)
+
+        def shiftchangecolor(mob, color, delta):
+            mob.set_color(WHITE)
+            mob.shift(delta)
+
+            return mob
+
+        self.play(
+            LaggedStart(
+                AnimationGroup(
+                    ApplyFunction(lambda x: shiftchangecolor(x, WHITE, LEFT * 0.75 + DOWN * 0.3), l3[0]),
+                    ApplyFunction(lambda x: shiftchangecolor(x, WHITE, delta), l3[1]),
+                ),
+                AnimationGroup(
+                    Write(lp),
+                    Write(rp),
+                    Write(cd),
+                ),
+                lag_ratio=0.7
+            )
+        )
+
+        p = Tex(r"$$\prod_{k=0}^{n-2}$$")
+        p.next_to(l3[0], LEFT).shift(UP * 0.08 + RIGHT * 0.13)
+        self.play(Write(p))
+
+        nto = Tex(r"\small $n^{n - 1}$")
+        nto.move_to((-nto[0].get_center() + l3[0][0].get_center())).shift(LEFT * 0.9).shift(UP * 0.12)
+
+        self.play(
+            LaggedStart(
+                AnimationGroup(
+                    p.animate.shift(RIGHT * 0.65),
+                    FadeOut(cd),
+                    Transform(l3[0], nto)
+                ),
+            )
+        )
+
+        fac = Tex(r"\small $(n - 1)!$")
+        fac.move_to(p).shift(RIGHT * 0.5 + DOWN * 0.072)
+
+        combine = VMobject()
+        combine.add(p)
+        combine.add(l3[1])
+        combine.add(lp)
+        combine.add(rp)
+
+        self.play(
+            Transform(combine, fac)
+        )
+
+        combine2 = VMobject()
+        combine2.add(combine)
+        combine2.add(nto)
+
+        eq = Tex(r"\small $=$").shift(DOWN * 0.8)
+
+        self.remove(nto)
+        self.remove(l3[0])
+
+        text_2 = Tex(r"$\kappa(n)$")
+        text_3d = Tex(r"$\cdot$").next_to(text_2, RIGHT)
+        text_3 = Tex(r"$n$").next_to(text_3d, RIGHT)
+        text_4d = Tex(r"$\cdot$").next_to(text_3, RIGHT)
+        text_4 = Tex(r"$(n-1)!$").next_to(text_4d, RIGHT)
+
+        left = VMobject()
+        left.add(text_2)
+        left.add(text_3d)
+        left.add(text_3)
+        left.add(text_4d)
+        left.add(text_4)
+        left.next_to(eq, LEFT)
+
+        self.play(
+            LaggedStart(
+                AnimationGroup(
+                    FadeOut(l),
+                    *[FadeOut(e) for e in edges],
+                    *[FadeOut(e) for e in numbers],
+                    FadeOut(g4),
+                ),
+                AnimationGroup(
+                    combine2.animate.next_to(eq, RIGHT),
+                ),
+                AnimationGroup(
+                    Write(left),
+                ),
+                AnimationGroup(
+                    Write(eq),
+                ),
+                lag_ratio=0.4,
+            ),
+        )
+
+        self.play(
+            LaggedStart(
+                AnimationGroup(
+                    FadeOut(combine),
+
+                    FadeOut(text_4d),
+                    FadeOut(text_4),
+                ),
+                AnimationGroup(
+                    text_2.animate.shift(RIGHT * 2.13),
+                    text_3d.animate.shift(RIGHT * 2.13),
+                    text_3.animate.shift(RIGHT * 2.13),
+                ),
+                lag_ratio=0.3,
+            ),
+        )
+
+        two = Tex("\small $^2$")
+        two.move_to(nto[0][3])
+
+        self.play(
+            LaggedStart(
+                AnimationGroup(
+                    Transform(nto[0][3], two),
+
+                    FadeOut(text_3d),
+                    FadeOut(text_3),
+                ),
+                AnimationGroup(
+                    text_2.animate.shift(RIGHT * 0.83),
+                ),
+                lag_ratio=0.3,
+            ),
+        )
+
+        o = Mobject()
+        o.add(text_2)
+        o.add(nto)
+
+        self.play(Circumscribe(o, color=WHITE))
