2011286b1d659a1930b562e05f97d603dc4dc681
xiaoxiae <tomas@slama.dev>
2021-11-01 16:50:31 +0100

    Tiles: a LOT of script and code

--- 09-bathroom-tiles/SCRIPT.md
+++ 09-bathroom-tiles/SCRIPT.md
@@ -5,17 +5,13 @@ FIRST SECTION NAME
 # Intro
 So you're taking a shower. In front of you, you see bathroom tiles that neatly fit together, creating a captivating pattern. The pattern is so interesting in fact that you think to yourself: can these be used for programming?
 
-Well, as it turns out, they can and are a topic of this video.
+Well, as it turns out, they can!
 
 
 # Definitions
-Formally, we'll picture the wall as a rectangle of width $w$ and height $h$ that we want to entirely fill with square tiles of size $1$. A tile will have 4 sides, each having some color.
+We'll picture the wall as a rectangle of width $w$ and height $h$ that we want to entirely fill with square tiles of size $1$. Each tile will have 4 sides with some colors.
 
-TODO: animace toho jak vzniká čtverec, jak tam jdou kostičky, jak každá má 4 strany
-
-For the tiling to make sense, we want the adjacent tiles' colors to match. We'll also make it so that each side of the wall has some color that again has to match the tiles it's adjacent to.
-
-TODO: animace toho, jak každá strana musí sednout k sobě
+For the tiling to make sense, we want the adjacent tiles' colors to match. We'll also make it so that each side of the wall has some color that again must match the tiles it's adjacent to.
 
 Now say we have some finite set of tile types and the colors of the wall. We can use as many tiles of each type as we want to create a tiling, but we can't rotate them. The question is: can we create a tiling that satisfies the requirements?
 
@@ -25,16 +21,16 @@ For some cases, it is pretty obvious that we either can or can't. Yet for others
 
 TODO: animace toho, pro které je to jasné a pro které ne, a pro které je to nejasné (například sudý počet jedné barvy)
 
-What we just described will be the programming model that we'll be using. We'll reserve the top side for input, which will be some finite sequence of colors. Our program will then be a finite set of tile types that we can use. Our program will **accept** the input if there exists a valid tiling of any non-zero height, and **reject** it if there's none.
+What we just described is the programming model that we'll be using. We'll reserve the top side for input, which will be some finite sequence of colors. Our program will then be a finite set of tile types that we can use. Our program will **accept** the input if there exists a valid tiling of some non-zero height, and **reject** it if there's none.
 
 But enough theory, let's see an example.
 
+Let's say we want to write a program (i. e. create a tile set) that accepts the input only if it contains all red or blue colors, but not both combined.
 
-Let's say we want to write a program (i. e. create a tile set) that accepts the input only if it contains only red or only blue color, not both.
+TODO:
+- reference to automatons
+- complexity by height
+- computational power
+- variations (triangles, hexagons)
+- 
 
-# Complexity
-
-	- horní bude vstup, široká podle vstupu, výška bude libovolná
-	- složitost: výška (s délkou vstupu)
-
-- počítání pouze jedniček a nul
--- 09-bathroom-tiles/scenes.py
+++ 09-bathroom-tiles/scenes.py
@@ -1,48 +1,89 @@
 from utilities import *
+from string import digits
+
+PALETTE = ["#b91e2f","#f68828","#cdd190","#122f30"]
+REDUCED_PALETTE = PALETTE[:2]
+
+
+def is_hex_color(str):
+    """Yeah I know, this is not pretty."""
+    return len(str) == 7 and str.startswith("#") and all([c in digits + "abcdef" for c in str.lower()[1:]])
+
 
 class Tile(VMobject):
-    def __init__(
-            self,
-            colors,
-            size=1,
-    ):
+    TEXT_SCALE = 0.6
+    TEXT_OFFSET = 1/3.5
+    DIRECTIONS = [RIGHT, UP, LEFT, DOWN]
+
+    def __init__(self, colors, size=1):
         super().__init__()
 
-        self.add(Square(size))
+        self.border = Square(size, color=WHITE)
+        self.add(self.border)
 
-        cross = VGroup()
-        cross.add(Line(start=(UP + RIGHT) * size / 2, end=(DOWN + LEFT) * size / 2, stroke_width=1))
-        cross.add(Line(end=(DOWN + RIGHT) * size / 2, start=(UP + LEFT) * size / 2, stroke_width=1))
+        self.size = size
 
-        self.add(cross)
+        self.colors = colors
 
-        for i, (color, direction) in enumerate(zip(colors, [RIGHT, UP, LEFT, DOWN])):
-            # TODO: maybe a better test for checking the color
-            triangle = Polygon(
-                np.array([size / 2, size / 2, 0]),
-                np.array([size / 2, -size / 2, 0]),
-                [0, 0, 0],
-                stroke_width=0,
-            ).rotate(PI / 2 * i, about_point=([0, 0, 0]))
+        self.cross = VGroup()
+        self.cross.add(Line(start=(UP + RIGHT) * self.size / 2, end=(DOWN + LEFT) * self.size / 2, stroke_width=1))
+        self.cross.add(Line(end=(DOWN + RIGHT) * self.size / 2, start=(UP + LEFT) * self.size / 2, stroke_width=1))
 
-            if len(color) == 7 and color.startswith("#"):
-                triangle.set_fill(color, 1)
-                self.add_to_back(triangle)
+        self.add(self.cross)
+
+        self.color_objects = VGroup()
+        for color, direction in zip(colors, Tile.DIRECTIONS):
+            self.set_color_in_direction(color, direction, new=True)
+
+        self.add_to_back(self.color_objects)
+
+    def set_color_in_direction(self, color, direction, new=False):
+        for i, d in enumerate(Tile.DIRECTIONS):
+            if (direction == d).all():
+                break
+
+        if not new:
+            self.colors[i] = color
+
+        triangle = Polygon(
+            np.array([self.size / 2 + self.get_x(), self.size / 2 + self.get_y(), 0]),
+            np.array([self.size / 2 + self.get_x(), -self.size / 2 + self.get_y(), 0]),
+            [self.get_x(), self.get_y(), 0],
+            stroke_width=1,
+            color=WHITE,
+        ).rotate(PI / 2 * i, about_point=([self.get_x(), self.get_y(), 0]))
+
+        if is_hex_color(color):
+            triangle.set_fill(color, 1)
+
+            if not new:
+                self.color_objects[i] = triangle
             else:
-                # TODO: duplication!
-                text = Tex(color).scale(0.6).scale(size).shift(direction / 3.5 * size)
-                self.add_to_back(text)
+                self.color_objects.add(triangle)
+        else:
+            text = Tex(color) \
+                .scale(Tile.TEXT_SCALE * self.size) \
+                .shift(direction * self.size * Tile.TEXT_OFFSET)
+
+            if not new:
+                self.color_objects[i] = text
+            else:
+                self.color_objects.add(text)
+
+    def get_color_in_direction(self, direction):
+        for i, d in enumerate(Tile.DIRECTIONS):
+            if (direction == d).all():
+                return self.colors[i]
+
+    def get_color_object_in_direction(self, direction):
+        """Return the color object in the given direction."""
+        for i, d in enumerate(Tile.DIRECTIONS):
+            if (direction == d).all():
+                return self.color_objects[i]
 
 
 class Wall(VMobject):
-    def __init__(
-            self,
-            colors,
-            input=None,  # if specified, the top color and width is ignored
-            width=3,
-            height=5,
-            size=1,
-    ):
+    def __init__(self, colors, input=None, width=3, height=5, size=1):
         super().__init__()
 
         # input overwrites the width
@@ -52,68 +93,299 @@ class Wall(VMobject):
         width *= size
         height *= size
 
-        self.add(Rectangle(width=width, height=height, stroke_width=2))
+        self.border = Rectangle(width=width, height=height)
+
+        self.size = size
+
+        self.tiles = VGroup()
+        self.add(self.tiles)
+
+        self.tile_position_dictionary = {}
+
+        self.add(self.border)
+
+        self.colors = colors
+
+        self.color_objects = VGroup()
 
         for i, (color, direction) in enumerate(zip(colors, [RIGHT, UP, LEFT, DOWN])):
             # yeah, not pretty
             # I was tired and didn't want to think
-            c = 0.2
+            c = 0.4  # outer offset
+            d = 0  # inner offset
 
             if (direction == UP).all():
                 pos = [
-                    np.array([width / 2, height / 2, 0]),
+                    np.array([width / 2, height / 2 + d, 0]),
                     np.array([width / 2 - c, height / 2 + c, 0]),
                     np.array([-width / 2 + c, height / 2 + c, 0]),
-                    np.array([-width / 2, height / 2, 0]),
+                    np.array([-width / 2, height / 2 + d, 0]),
                 ]
             if (direction == DOWN).all():
                 pos = [
-                    np.array([width / 2, -height / 2, 0]),
+                    np.array([width / 2, -height / 2 - d, 0]),
                     np.array([width / 2 - c, -height / 2 - c, 0]),
                     np.array([-width / 2 + c, -height / 2 - c, 0]),
-                    np.array([-width / 2, -height / 2, 0]),
+                    np.array([-width / 2, -height / 2 - d, 0]),
                 ]
             if (direction == LEFT).all():
                 pos = [
-                    np.array([-width / 2, height / 2, 0]),
+                    np.array([-width / 2 - d, height / 2, 0]),
                     np.array([-width / 2 - c, height / 2 - c, 0]),
                     np.array([-width / 2 - c, -height / 2 + c, 0]),
-                    np.array([-width / 2, -height / 2, 0]),
+                    np.array([-width / 2 - d, -height / 2, 0]),
                 ]
             if (direction == RIGHT).all():
                 pos = [
-                    np.array([width / 2, height / 2, 0]),
+                    np.array([width / 2 + d, height / 2, 0]),
                     np.array([width / 2 + c, height / 2 - c, 0]),
                     np.array([width / 2 + c, -height / 2 + c, 0]),
-                    np.array([width / 2, -height / 2, 0]),
+                    np.array([width / 2 + d, -height / 2, 0]),
                 ]
 
             if (direction == UP).all() and input is not None:
-                c = 0.3
                 for i in range(len(input) + 1):
-                    line = Line(start=[-width / 2 + i, height / 2, 0], end=[-width / 2 + i, height / 2 + c, 0], stroke_width=2)
-
+                    c = (0.25 if i in (0, len(input)) else 0.5) * self.size
+                    line = Line(start=[-width / 2 + i * self.size, height / 2, 0], end=[-width / 2 + i * self.size, height / 2 + c, 0])
                     self.add(line)
 
-                    # TODO: duplication!
                     if i < len(input):
-                        # TODO: YUCK! first fix the duplication
-                        text = Tex(input[i]).scale(0.6).scale(size).move_to([-width / 2 + i + 0.5, height / 2, 0]).align_to([-width / 2 + i + 0.5, height / 2, 0], DOWN)
+                        p = [-width / 2 + (i + 0.5) * self.size, height / 2, 0]
+
+                        text = Tex(input[i]) \
+                            .scale(Tile.TEXT_SCALE * self.size) \
+                            .move_to(p) \
+                            .align_to(p, DOWN) \
+                            .shift(UP * Tile.TEXT_OFFSET / 2)  # 2 is an eyeball magic constant
                         self.add(text)
             else:
-                side = Polygon(*pos).set_fill(color, 1).set_stroke(WHITE)
-                self.add_to_back(side)
+                side = Polygon(*pos).set_stroke(WHITE)
+                if is_hex_color(color):
+                    self.color_objects.add_to_back(side.set_fill(color, 1))
+                else:
+                    self.color_objects.add_to_back(side)
+
+                    text = Tex(color) \
+                        .scale(Tile.TEXT_SCALE * self.size) \
+                        .move_to(side)
+
+                    self.color_objects.add_to_back(text)
+
+        self.add(self.color_objects)
+
+    def to_positive_coordinates(self, x, y):
+        """Done so we can use -1."""
+        return int(x % self.border.width), int(y % self.border.height)
+
+    def index_to_position(self, x, y):
+        x, y = self.to_positive_coordinates(x, y)
+
+        return Square(self.size) \
+                .align_to(self.border, UP + LEFT) \
+                .shift([x * self.size, -y * self.size, 0]) \
+                .get_center()
+
+    def add_tile(self, tile, x, y, copy=False):
+        x, y = self.to_positive_coordinates(x, y)
+
+        if copy:
+            tile = tile.copy()
+
+        tile.move_to(self.index_to_position(x, y))
+
+        self.tiles.add(tile)
+
+        # TODO: sort by tile position dictionary!
+
+        self.tile_position_dictionary[(x, y)] = tile
+
+        return tile
+
+    def get_tile(self, x, y):
+        x, y = self.to_positive_coordinates(x, y)
+
+        return self.tile_position_dictionary[(x, y)]
+
+    get_color_in_direction = Tile.get_color_in_direction
+    get_color_object_in_direction = Tile.get_color_object_in_direction
 
 
-class WriteTile(Write):
-    """A special write for a tile (since we want the animation to be reversed)."""
+class TileSet(VMobject):
+    def __init__(self, *tiles):
+        super().__init__()
+
+        self.tiles = VGroup(*tiles)
+        self.tiles.arrange_in_grid(rows=1, buff=0.6)
+
+        self.commas = VGroup()
+
+        n = len(tiles)
+        for i in range(n - 1):
+            self.commas.add(Tex("\Large $,$").next_to(self.tiles[i], DOWN + RIGHT, buff=0).shift(UP * 0.07 + RIGHT * 0.1))
+
+        brace_offset = -0.25
+        self.braces = VGroup(
+            BraceBetweenPoints(
+                Point().next_to(self.tiles[0], UP + LEFT).get_center(),
+                Point().next_to(self.tiles[0], DOWN + LEFT).get_center(),
+                direction=LEFT,
+            ).shift(LEFT * brace_offset),
+            BraceBetweenPoints(
+                Point().next_to(self.tiles[-1], UP + RIGHT).get_center(),
+                Point().next_to(self.tiles[-1], DOWN + RIGHT).get_center(),
+                direction=RIGHT,
+            ).shift(RIGHT * brace_offset),
+        )
+
+        self.add(self.tiles)
+        self.add(self.commas)
+        self.add(self.braces)
+
+
+
+
+
+class WriteReverse(Write):
+    """A special write for a tile and wall (since we want the animation to be reversed)."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, reverse=True)
 
 
+
 class Intro(Scene):
     def construct(self):
-        tile = Tile(["a", RED, "B", BLUE]).shift(LEFT * 2)
-        wall = Wall([RED, GREEN, ORANGE, YELLOW], input="abc").shift(RIGHT * 2)
+        w = 8
+        h = 5
 
-        self.play(WriteTile(tile), Write(wall), reverse=True)
+        wall = Wall(PALETTE, width=w, height=h)
+
+        bl = BraceBetweenPoints([-w/2, h/2, 0], [-w/2, -h/2, 0])
+        blt = Tex("\\footnotesize h").next_to(bl, LEFT * 0.5)
+
+        bu = BraceBetweenPoints([-w/2, h/2, 0], [w/2, h/2, 0], direction=UP).scale([-1, 1, 0])
+        but = Tex("\\footnotesize w").next_to(bu, UP * 0.5)
+
+        self.play(Write(wall.border))
+
+        self.play(Write(bu), Write(but))
+        self.play(Write(bl), Write(blt))
+
+        tiles = [[Tile([choice(PALETTE) for _ in range(4)]) for _ in range(w)] for _ in range(h)]
+
+        for i in range(w):
+            for j in range(h):
+                wall.add_tile(tiles[j][i], i, j)
+
+        self.play(AnimationGroup(*[FadeIn(t.border) for t in wall.tiles], lag_ratio=0.01))
+
+        for t in wall.tiles:
+            for c in t.color_objects:
+                self.bring_to_back(c)
+            self.bring_to_front(t)
+
+        self.play(
+            *[t.border.animate.set_color(WHITE) for t in wall.tiles],
+            *[Write(t.cross) for t in wall.tiles],
+            *[Write(t.color_objects) for t in wall.tiles],
+        )
+
+        # color rows + columns
+        changes = {}
+        seed(1)
+        for count, (i1, i2) in enumerate(zip((range(w-1), range(w)), (range(h), range(h-1)))):
+            for i in i1:
+                for j in i2:
+                    p1, p2 = (i, j), (i + 1, j) if not count else (i, j + 1)
+                    d1, d2 = (RIGHT, LEFT) if not count else (DOWN, UP)
+
+                    c1, c2 = tiles[p1[1]][p1[0]].get_color_in_direction(d1), tiles[p2[1]][p2[0]].get_color_in_direction(d2)
+                    new_color=choice([c1, c2])
+
+                    if p1 not in changes:
+                        changes[p1] = tiles[p1[1]][p1[0]].animate
+                    changes[p1].set_color_in_direction(new_color, d1)
+
+                    if p2 not in changes:
+                        changes[p2] = tiles[p2[1]][p2[0]].animate
+                    changes[p2].set_color_in_direction(new_color, d2)
+        self.play(*changes.values())
+        self.play(AnimationGroup(FadeOut(VGroup(bl, blt, bu, but)), FadeIn(wall.color_objects), lag_ratio=0.4))
+
+        changes = {}
+
+        for count, iterable in enumerate([range(w), range(h)]):
+            for x in iterable:
+                for index in range(2):
+                    y = ((0, h - 1) if not count else (0, w - 1))[index]
+                    direction = ((UP, DOWN) if not count else (LEFT, RIGHT))[index]
+
+                    if count:
+                        i, j = y, x
+                    else:
+                        i, j = x, y
+
+                    if tiles[j][i].get_color_in_direction(direction) != wall.get_color_in_direction(direction):
+
+                        if (i, j) not in changes:
+                            changes[(i, j)] = tiles[j][i].animate
+                        changes[(i, j)].set_color_in_direction(wall.get_color_in_direction(direction), direction)
+
+        self.play(*changes.values())
+
+        self.play(*[FadeOut(o) for o in self.mobjects])
+
+
+class TileSetExample(Scene):
+    def construct(self):
+        seed(2)
+        tiles = [Tile([choice(REDUCED_PALETTE) for _ in range(4)]) for _ in range(5)]
+
+        tiles = VGroup(*tiles)
+        tiles.arrange_in_grid(rows=1)
+
+        tileset = TileSet(*tiles)
+
+        self.play(
+                # TODO: move this to some function?
+                AnimationGroup(
+                    AnimationGroup(*[Write(t) for t in tileset.tiles], lag_ratio=0.1),
+                    ),
+                    Write(tileset.commas),
+                    Write(tileset.braces),
+                    lag_ratio=0.5
+                )
+
+        tileTypes = Tex("Tile Types $=$").shift(LEFT * 3)
+
+        tileset_with_text = VGroup(tileTypes, tileset)
+
+        self.play(
+            FadeIn(tileTypes),
+            tileset_with_text.animate.arrange_in_grid(rows=1, buff=0.23).move_to([0, 0, 0])
+        )
+
+        w = 8
+        h = 3
+        wall = Wall(REDUCED_PALETTE * 2, width=w, height=h)
+
+        wall.shift(DOWN * 1.2)
+
+        self.play(
+            AnimationGroup(
+                tileset_with_text.animate.shift(UP * 2.3),
+                Write(wall),
+                lag_ratio=0.6,
+            )
+        )
+
+        tile_dots = [Dot().set_opacity(0).move_to(tiles[-1]) for _ in range(h)]
+        tile_copies = [tiles[-1].copy().move_to(wall.index_to_position(-1, i)) for i in range(h)]
+
+        self.play(
+            AnimationGroup(
+                *[FadeTransform(tile_dots[i], tile_copies[i]) for i in range(h)],
+                lag_ratio=0.2,
+            )
+        )
+
+        self.wait(3)
