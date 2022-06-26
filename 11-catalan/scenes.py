6e923a29aea35869a04a15c4829595659e4a27bf
xiaoxiae <tomas@slama.dev>
2022-06-26 10:46:53 +0200

    Catalan: better stars and binary trees code

--- 11-catalan/scenes.py
+++ 11-catalan/scenes.py
@@ -3,18 +3,21 @@ from utilities import *
 
 class GraphStarsScene(Scene):
     def construct(self):
-        trees = VGroup(*generate_binary_trees(3)).arrange(buff=0.8)
+        trees = VGroup(*BinaryTree.generate_binary_trees(3)).arrange(buff=0.8)
 
         self.play(AnimationGroup(*[Write(t) for t in trees], lag_ratio=0.1))
 
-        stars_anim = []
+        stars_animations = []
         for t in trees:
-            stars_anim.append(AnimationGroup(*AnimateStars(t, add_stars(t)), lag_ratio=0.1))
+            StarUtilities.add_stars_to_graph(t)
+            stars_animations.append(CreateStars(t))
 
-        self.play(*stars_anim)
+        self.play(*stars_animations)
 
         self.wait(1)
 
         self.play(*SwapChildren(trees[2], ""))
 
         self.wait(1)
+
+        self.play(ChangeStars(trees[2], 5))
--- 11-catalan/utilities.py
+++ 11-catalan/utilities.py
@@ -1,157 +1,113 @@
+from __future__ import annotations
+
 from manim import *
 from typing import *
+from itertools import permutations
 
 
+class StarUtilities:
+    @classmethod
+    def get_star_name(cls, i):
+        return f"star_{i}"
 
-def get_non_full_vertices(graph: Graph):
-    """Return all vertices of a graph (having '0' as root) that don't have 2 children."""
-    vertices = []
+    @classmethod
+    def is_star_name(cls, s):
+        return "star" in s
 
-    for v in graph.vertices:
+    @classmethod
+    def get_star_index(cls, s):
+        return int(s[len("star_"):])
 
-        count = 0
-        for w, _ in graph.edges:
-            if v == w:
-                count += 1
+    @classmethod
+    def create_star_object(cls, number) -> VMobject:
+        star_scale = 0.2
 
-        if count != 2:
-            vertices += [v]
+        # this MUST be an odd number, else the rotation won't work
+        n = 6
+        star = Star(n, density=1.5, color=BLUE, fill_opacity=1).scale(star_scale).rotate(PI * 2 / (n * 2))
+        text = Tex(str(number), color=BLACK).scale(2.5 * star_scale).move_to(star)
 
-    return vertices
+        return VGroup(star, text)
 
-
-def generate_binary_trees(n: int):
-    def _generate(n: int) -> List:
-        """Generate all binary trees on n vertices.
-        n = 1: [None, None]
-        n = 2: [[None, None], None] and [None, [None, None]]
-        n = 3: ..."""
-
-        if n == 0:
-            return None
-
-        else:
-            previous_trees = []
-            trees = []
-
-            for i in range(n):
-                previous_trees.append(_generate(i))
-
-            for i in range(n):
-                for l1 in (previous_trees[i] or [None]):
-                    for l2 in (previous_trees[n - i - 1] or [None]):
-                        trees.append([l1, l2])
-
-            return trees
-
-    return [generate_binary_tree(tree) for tree in _generate(n)]
-
-
-def generate_binary_tree(graph) -> VMobject:
-    """Generate a binary tree from nested parentheses.
-    Sample input: [None, [None, None]] (generating a 2-vertex binary tree)."""
-
-    vertices = [""]
-    edges = []
-
-    def _generate(g, v):
-        """Populate vertices and edges. Vertices to be deleted contain the 'B' symbol."""
-        if g == [None, None]:
-            return
-
-        for i in range(2):
-            s = "l" if i == 0 else "r"
-
-            if g[i] != None:
-                vertices.append(v + s)
-                edges.append((v, vertices[-1]))
-
-                _generate(g[i], vertices[-1])
-            else:
-                vertices.append(v + s + "B")
-                edges.append((v, vertices[-1]))
-
-    _generate(graph, "")
-
-    g = Graph(
-        vertices,
-        edges,
-        layout="tree",
-        root_vertex="",
-        layout_config={"vertex_spacing": (1.15, 1)},
-        vertex_type=lambda: Dot().scale(3),
-    )
-
-    for v in list(g.vertices):
-        if "B" in v:
-            g.remove_vertices(v)
-
-    return g
-
-
-def star_name(i):
-    return f"star_{i}"
-
-def add_stars(graph) -> List[str]:
-    """Add stars to the graph, returning their names. It is assumed that '' is the root."""
-
-    def create_star(v, number, direction, direction_scale=0.26, color=BLUE):
+    @classmethod
+    def attach_star_to_vertex(cls, graph, v, number, direction, direction_scale=0.26):
         """Create star from the vertex in the given direction (LEFT/RIGHT)."""
 
-        def create_star_object():
-            star_scale = 0.2
-
-            star = Star(5, density=1.5, color=color, fill_opacity=1).scale(star_scale)
-            text = Tex(str(number), color=BLACK).scale(2.5 * star_scale).move_to(star).shift(DOWN * star_scale * 0.12)
-
-            return VGroup(star, text)
-
-        graph.add_vertices(
-            star_name(number),
+        vertex = graph.add_vertices(
+            cls.get_star_name(number),
             positions={
-                star_name(number): graph.vertices[v].get_center()
+                cls.get_star_name(number): graph.vertices[v].get_center()
                 + (DOWN * 1.9 + direction) * direction_scale
             },
-            vertex_type=create_star_object,
+            vertex_type=lambda: cls.create_star_object(number),
         )[0]
 
         graph.add_edges(
-            ((v, star_name(number))),
-            edge_type=lambda *args, **kwargs: Line(*args, color=color, **kwargs),
+            ((v, cls.get_star_name(number))),
+            edge_type=lambda *args, **kwargs: Line(*args, color=vertex.color, **kwargs),
         )
 
-    count = 0
 
-    # add stars sorted from left to right (so by the x, y coordinate of parent)
-    for v in sorted(get_non_full_vertices(graph), key = lambda v: (graph.vertices[v].get_center()[0], graph.vertices[v].get_center()[1])):
-        descendants = [x for (w, x) in graph.edges if v == w]
+    @classmethod
+    def add_stars_to_graph(cls, graph: BinaryTree):
+        """Add stars to a binary tree. It is assumed that '' is the root."""
 
-        # add both stars
-        if len(descendants) == 0:
-            create_star(v, count + 1, LEFT)
-            create_star(v, count + 2, RIGHT)
-            count += 2
+        count = 0
 
-        # add one star
-        else:
-            d = descendants[0]
+        # add stars sorted from left to right (so by the x, y coordinate of parent)
+        for v in sorted(graph.get_non_full_vertices(), key = lambda v: (graph.vertices[v].get_center()[0], graph.vertices[v].get_center()[1])):
+            descendants = [x for (w, x) in graph.edges if v == w]
 
-            if d[-1] == "l":
-                create_star(v, count + 1, LEFT)
+            # add both stars
+            if len(descendants) == 0:
+                cls.attach_star_to_vertex(graph, v, count + 1, LEFT)
+                cls.attach_star_to_vertex(graph, v, count + 2, RIGHT)
+                count += 2
+
+            # add one star
             else:
-                create_star(v, count + 1, RIGHT)
+                d = descendants[0]
 
-            count += 1
+                if d[-1] == "l":
+                    cls.attach_star_to_vertex(graph, v, count + 1, LEFT)
+                else:
+                    cls.attach_star_to_vertex(graph, v, count + 1, RIGHT)
 
-    return [star_name(i + 1) for i in range(count)]
+                count += 1
+
+        return [cls.get_star_name(i + 1) for i in range(count)]
 
 
-def AnimateStars(graph, star_names) -> List[Animation]:
-    """Return a list of animations of creating all of the stars of a graph."""
+def ChangeStars(graph, permutation: int) -> Animation:
+    """Return the animation of changing the permutation on the stars."""
+    n = (len(graph.vertices) + 1) // 2
+    p = list(permutations(range(1, n + 1)))[permutation]  # computers are fast
+
+    animations = []
+
+    for vertex in graph.vertices:
+        if StarUtilities.is_star_name(vertex):
+            star = graph.vertices[vertex]
+
+            new_index = p[StarUtilities.get_star_index(vertex) - 1]
+
+            if StarUtilities.get_star_index(vertex) != new_index:
+                new_star = StarUtilities.create_star_object(new_index)
+                new_star[1].move_to(star[0])
+
+                animations += [
+                    star[0].animate(rate_func=there_and_back).scale(0.75),
+                    star[1].animate.become(new_star[1]),
+                ]
+
+    return AnimationGroup(*animations)
+
+def CreateStars(graph) -> Animation:
+    """Return an animation of creating all of the stars of a graph."""
     animations = []
 
     for (u, v) in graph.edges:
-        if v in star_names:
+        if StarUtilities.is_star_name(v):
             animations.append(
                 (
                     graph.vertices[v].get_center()[0],
@@ -170,7 +126,99 @@ def AnimateStars(graph, star_names) -> List[Animation]:
                 )
             )
 
-    return [a for (_, a) in sorted(animations, key=lambda x: x[0])]
+    return AnimationGroup(*[a for (_, a) in sorted(animations, key=lambda x: x[0])], lag_ratio=0.1)
+
+
+class BinaryTree(Graph):
+
+    def get_non_full_vertices(self) -> List[str]:
+        """Return all vertices of a graph (having '0' as root) that don't have 2 children."""
+        vertices = []
+
+        for v in self.vertices:
+
+            count = 0
+            for w, _ in self.edges:
+                if v == w:
+                    count += 1
+
+            if count != 2:
+                vertices += [v]
+
+        return vertices
+
+    @classmethod
+    def _generate_parentheses(cls, n: int) -> Optional[List]:
+        """Generate all nested parentheses representing binary trees on n vertices.
+        n = 1: [None, None]
+        n = 2: [[None, None], None] and [None, [None, None]]
+        n = 3: ..."""
+
+        if n == 0:
+            return None
+
+        else:
+            previous_trees = []
+            trees = []
+
+            for i in range(n):
+                previous_trees.append(cls._generate_parentheses(i))
+
+            for i in range(n):
+                for l1 in (previous_trees[i] or [None]):
+                    for l2 in (previous_trees[n - i - 1] or [None]):
+                        trees.append([l1, l2])
+
+            return trees
+
+    @classmethod
+    def _binary_tree_from_parentheses(cls, graph) -> VMobject:
+        """Generate a binary tree from nested parentheses.
+        Sample input: [None, [None, None]] (generating a 2-vertex binary tree)."""
+
+        vertices = [""]
+        edges = []
+
+        def _populate(g, v):
+            """Populate vertices and edges. Vertices to be deleted contain the 'B' symbol."""
+            if g == [None, None]:
+                return
+
+            for i in range(2):
+                s = "l" if i == 0 else "r"
+
+                if g[i] != None:
+                    vertices.append(v + s)
+                    edges.append((v, vertices[-1]))
+
+                    _populate(g[i], vertices[-1])
+                else:
+                    vertices.append(v + s + "B")
+                    edges.append((v, vertices[-1]))
+
+        _populate(graph, "")
+
+        g = cls(
+            vertices,
+            edges,
+            layout="tree",
+            root_vertex="",
+            layout_config={"vertex_spacing": (1.15, 1)},
+            vertex_type=lambda: Dot().scale(3),
+        )
+
+        for v in list(g.vertices):
+            if "B" in v:
+                g.remove_vertices(v)
+
+        return g
+
+    @classmethod
+    def generate_binary_trees(cls, n: int) -> List[VMobject]:
+        """Generate all binary trees on n vertices."""
+        return [cls._binary_tree_from_parentheses(p)
+                for p in cls._generate_parentheses(n)]
+
 
 
 def SwapChildren(graph, v, move_distance=0.1, speed_ratio=0.75, **kwargs) -> List[Animation]:
@@ -227,6 +275,7 @@ def SwapChildren(graph, v, move_distance=0.1, speed_ratio=0.75, **kwargs) -> Lis
     # find the children
     a, b = [x for (w, x) in graph.edges if v == w]
 
+    # TODO: using AnimationGroup here stops updaters, I'm not sure why
     return [
         *generate_swap_animations(v, a, UP),
         *generate_swap_animations(v, b, DOWN),
