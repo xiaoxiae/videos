5a21a581367b164d4d047ddefc1d9c2947e10cb7
xiaoxiae <tomas@slama.dev>
2022-10-24 15:47:23 +0200

    templates: shorts have different background

--- 00-template-short/scenes.py
+++ 00-template-short/scenes.py
@@ -1,8 +1,10 @@
 from manim import *
 
 
-class Intro(Scene):
+class Intro(MovingCameraScene):
     def construct(self):
+        self.camera.background_color = DARKER_GRAY
+
         text = Tex("\Huge Intro")
 
         self.play(Write(text))
