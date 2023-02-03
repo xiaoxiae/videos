aa3f30f97a80753df2af8112dcd60cf932299b64
xiaoxiae <tomas@slama.dev>
2023-02-03 18:09:31 +0100

    Short Tutte: essentially finished?

--- 16-tutte-short/SCRIPT.md
+++ 16-tutte-short/SCRIPT.md
@@ -1,5 +1,5 @@
 ---
-title: <title>
+title: Undirected graphs can't equal a polynomial... right?
 subtitle: Video Script
 author: Tomáš Sláma
 header-includes:
@@ -9,18 +9,20 @@ header-includes:
 \hrule
 \vspace{1.5em}
 
-If I gave you a graph and told you to calculate different ways of coloring its vertices such that adjacent vertices have different colors, what would you do?
+If I gave you this graph and told you to calculate all of its vertex colorings using $n$ colors, what would you do?
 
-You could just bruteforce the solution but that's too boring.
-Instead, we'll calculate what is called a chromatic polynomial for this graph.
+You could try to bruteforce the solution, but that would only work for smaller $n$s.
+Instead, we can calculate what is called a chromatic polynomial for this graph.
 If we plug in $n$ for any $x$, we get the exact number of colorings -- cool, right?
 
-This, however, is just a specialized form of the much more interesting Tutte polynomial.
+This, however, is just a form of the much more interesting Tutte polynomial.
 This two-variate polynomial is fascinating because
 
 - $x=1, y=1$ counts the number of spanning trees,
 - $x=2, y=0$ counts the number of acyclic orientations
 
-and much more -- it is used for measuring network reliability, for calculating linear codes and even has ties to quantum field theory.
+and much more -- it is used for measuring network reliability, for calculating linear codes and even has ties to quantum field theory. <!-- fade in more list -->
 
-If you found this interesting, I'd recommend you check out this neat handbook, written by many experts in the field, which contains a great overview of this polynomial's many fascinating uses.
+If you found this interesting and want to know more (like how to actually calculate it), I'd recommend you check out this neat handbook, written by many experts in the field, which contains a great overview of this polynomial's many fascinating uses.
+
+In any case, thanks for watching!
--- 16-tutte-short/scenes.py
+++ 16-tutte-short/scenes.py
@@ -1,7 +1,7 @@
 from manim import *
-from random import seed, shuffle
+from random import seed, shuffle, uniform
 from math import comb
-from itertools import combinations
+from itertools import combinations, product
 import sympy
 from sympy.abc import x, y
 import networkx as nx
@@ -111,15 +111,26 @@ def polynomial_to_tex(p):
     return sympy.latex(p.as_expr().expand())
 
 
-class Intro(MovingCameraScene):
-    def construct(self):
-        seed(0xdeadbeef)
+def get_all_colorings(V, E, n):
+    """Return a dictionary of all vertex colorings for a given graph."""
+    colors = [RED, GREEN, BLUE, PINK, ORANGE, LIGHT_BROWN]
 
-        self.camera.background_color = DARKER_GRAY
+    def _is_valid(assignment):
+        for u, v in E:
+            if assignment[u] == assignment[v]:
+                return False
+        return True
 
-        g_nx = nx.windmill_graph(3, 3)
+    assignments = []
+    for assignment in product(colors[:n], repeat=len(V)):
+        if _is_valid(assignment):
+            assignments.append(assignment)
 
-        # TODO:
+    return assignments
+
+
+class Introo(MovingCameraScene):
+    def get_graphy_graphy(self, g_nx):
         ax = Axes(
             x_range=[-0.25, 3, 1],
             y_range=[-5, 24, 1],
@@ -134,30 +145,49 @@ class Intro(MovingCameraScene):
         )
 
         f = lambda i: float(chromatic(g_nx.nodes, g_nx.edges).subs(x, i))
-        graph = ax.plot(f, x_range=[-0.35, 3.10, 0.01])
+        graph = ax.plot(f, x_range=[-0.35, 3.10, 0.1])  # TODO: value 0.01
 
         a = Dot(ax.c2p(0, 0)).scale(1.5).set_z_index(1)
         b = Dot(ax.c2p(1, 0)).scale(1.5).set_z_index(1)
         c = Dot(ax.c2p(2, 0)).scale(1.5).set_z_index(1)
         d = Dot(ax.c2p(3, 24)).scale(1.5).set_z_index(1)
 
-        x_label = ax.get_x_axis_label("x")
+        bn = Tex("0").next_to(b, UP).set_z_index(1)
+        cn = Tex("0").next_to(c, UP).set_z_index(1)
+        dn = Tex("24").next_to(d, LEFT).set_z_index(1)
+
+        x_label = ax.get_x_axis_label("n")
 
         graph = CurvesAsSubmobjects(graph).set_color_by_gradient(DARKER_GRAY,
                                                                  WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE, WHITE,
                                                                  DARKER_GRAY)
 
-        self.play(
-            FadeIn(ax),
-            FadeIn(x_label),
-            Succession(Wait(0.25), Write(graph, run_time=1)),
-            AnimationGroup(
-                *[FadeIn(o) for o in [a, b, c, d]],
-                lag_ratio=0.25
-            )
-        )
+        return ax, x_label, graph, a, b, c, d, bn, cn, dn
 
-        return
+
+    def construct(self):
+        seed(0xdeadbeef)
+
+        g_nx = nx.windmill_graph(3, 3)
+
+        p = Tex(r"""$$
+\begin{aligned}
+T(G) &= x^{6} + 3 x^{5} \\[-0.1cm]
+    &+ 3 x^{4} y + 3 x^{4} \\[-0.1cm]
+    &+ 6 x^{3} y + x^{3} \\[-0.1cm]
+    &+ 3 x^{2} y^{2} + 3 x^{2} y \\[-0.1cm]
+    &+ 3 x y^{2} + y^{3}
+\end{aligned}$$""").scale(2)
+
+        good_for = Tex(r"""
+        \begin{itemize} \item $T_{1,1} = $ \# of spanning trees
+                        \item $T_{2,0} = $ \# of acyclic orientations
+                        \item measures network reliability
+                        \item calculates linear codes
+                        \item has ties to quantum field theory
+                       \end{itemize}""")
+
+        colorings = get_all_colorings(g_nx.nodes, g_nx.edges, 3)
 
         g = Graph.from_networkx(g_nx, layout="spring", layout_scale=0.8, layout_config={'seed': 1})\
                 .scale(5)\
@@ -169,8 +199,10 @@ class Intro(MovingCameraScene):
         random_vertices = list(g.vertices)
         shuffle(random_vertices)
 
-        a = Tex(r"\underline{How many colorings?}").scale(2.25)
-        b = Tex(r"(adjacent vertices $\iff$ different colors)").scale(1.25).next_to(a, DOWN, buff=0.7)
+        a = Tex(r"\underline{How many $n$-colorings?}").scale(2.15)
+        b = Tex(r"(adjacent vertices $=$ different colors)").scale(1.25).next_to(a, DOWN, buff=0.7)
+
+        texts = VGroup(a, b)
 
         spacing = 3
 
@@ -200,22 +232,232 @@ class Intro(MovingCameraScene):
             ),
         )
 
-        self.play()
+        graph_grid = VGroup()
 
-        return
+        for c in colorings:
+            gc = g.copy()
 
-        c = Tex(f"$$\chi(G) = {polynomial_to_tex(chromatic(g_nx.nodes, g_nx.edges))}$$")
-        self.play(FadeIn(c))
+            graph_grid.add(gc)
 
-        #formula = Tex(r"\chi(G) = ")
+            for i, vertex in enumerate(gc.vertices):
+                gc.vertices[vertex].set_color(c[i]).scale(2)
 
-        ##p = tutte(g.nodes, g.edges())
-        #
-        #p = tutte([0, 1, 2, 3, 4], [(0, 1), (1, 2), (1, 3), (2, 3), (3, 4)])
-        #c = chromatic([0, 1, 2, 3, 4], [(0, 1), (1, 2), (1, 3), (2, 3), (3, 4)])
-        #print(str(p))
-        #print(str(c))
-        #print(p.subs(y, 0).subs(x, 1))
-        #quit()
+        graph_grid.scale(0.125).arrange_in_grid(cols=4, buff=0.2)
+
+        box = SurroundingRectangle(graph_grid, color=GRAY, buff=0.25, corner_radius=0.2, stroke_width=5)
+
+        ax, x_label, graph, a, b, c, d, bn, cn, dn = self.get_graphy_graphy(g_nx)
+
+        d.set_z_index(1)
+        dn.set_z_index(1)
+
+        beeg_g = VGroup(ax, x_label, graph, a, b, c, d, bn, cn, dn)
+        beeg_g.scale(0.9).align_to(g, DOWN).shift(DOWN * 4)  # god forgive me
+
+        line = Line(
+                start=Dot().scale(0.00001).next_to(box, RIGHT, buff=0).shift(DOWN * 0.5).get_center(),
+                end=d.get_center(),
+                color=GRAY,
+        )
+
+        c = Tex(f"$$\chi(G) = {polynomial_to_tex(chromatic(g_nx.nodes, g_nx.edges))}$$").next_to(beeg_g, DOWN, buff=-0.2)
+
+        c[0][2].set_opacity(0)
+        cin = g.copy().set_color(WHITE).move_to(c[0][2]).set_height(c[0][2].get_height())
+
+        self.play(
+            self.camera.frame.animate.move_to(VGroup(g, beeg_g, texts, c)),
+            FadeIn(ax),
+            FadeIn(x_label),
+            Succession(Wait(0.25), Write(graph, run_time=1)),
+            Succession(Wait(0.25), FadeIn(c, run_time=1)),
+            Succession(Wait(0.5), FadeIn(cin, run_time=1)),
+            AnimationGroup(
+                *[FadeIn(o) for o in [a, b, c, d]],
+                lag_ratio=0.25
+            ),
+            AnimationGroup(
+                *[FadeIn(o) for o in [Dot().shift(LEFT * 1000), bn, cn, dn]],  # xd
+                lag_ratio=0.25
+            ),
+        )
+
+        t = 8
+        self.play(
+            AnimationGroup(
+                Transform(g, graph_grid[t]),
+                AnimationGroup(
+                    FadeIn(graph_grid[:t]),
+                    FadeIn(graph_grid[t:]),
+                    FadeIn(box),
+                    FadeIn(line),
+
+                ),
+                lag_ratio=0.5,
+            ),
+        )
+
+        p[0][2].set_opacity(0)
+        p.next_to(c, DOWN, buff=4)
+        idkman = g.copy().set_color(WHITE).move_to(p[0][2]).set_height(p[0][2].get_height()).shift(DOWN * 0.04)
+
+        self.play(
+            AnimationGroup(
+                self.camera.frame.animate.next_to(self.camera.frame, DOWN, buff=-2),
+                AnimationGroup(
+                    FadeIn(p),
+                    FadeIn(idkman, run_time=1),
+                ),
+                lag_ratio=0.5,
+            ),
+        )
+
+        good_for.scale(1.25).next_to(p, DOWN, buff=2).align_to(p, LEFT)
+
+        good_for[0][0:22].set_color(RED)
+        good_for[0][22:50].set_color(BLUE)
+
+        self.play(FadeIn(good_for[0][0:22]))
+        self.play(FadeIn(good_for[0][22:50]))
+
+        s = 11
+
+        seed(s)
+        nxgraph = nx.erdos_renyi_graph(12, 0.45, seed=s)
+        G = Graph.from_networkx(nxgraph, layout="spring", layout_scale=3.5, layout_config={'seed': s}).rotate(PI / 7 + PI).scale(1.15)
+
+        for e in G.edges:
+            G.edges[e].set_stroke_width(uniform(1, 7))
+            G.edges[e].set_opacity(uniform(0, 1))
+        for v in G.vertices:
+            G.vertices[v].scale(uniform(2, 3.5))
+
+        G.next_to(p, DOWN, buff=7)
+
+        self.play(
+            FadeIn(good_for[0][50:77]),
+            FadeIn(G),
+        )
+
+        self.play(
+            FadeOut(G),
+        )
+
+        quantum = ImageMobject("assets/index.png").set_width(self.camera.frame.get_width() * 0.5)
+        quantum.next_to(p, DOWN, buff=8.4)
+
+        lc = Tex(r"""$$\begin{pmatrix}
+            1 & 1 & 0 & 1 & 0 & 0 & 0 \\
+            0 & 1 & 1 & 0 & 1 & 0 & 0 \\
+            0 & 0 & 1 & 1 & 0 & 1 & 0 \\
+            0 & 0 & 0 & 1 & 1 & 0 & 1
+        \end{pmatrix}$$""").scale(1.5)
+
+        lc.next_to(p, DOWN, buff=8.4)
+
+        self.play(
+            FadeIn(good_for[0][77:99]),
+            FadeIn(lc),
+        )
+
+        self.play(
+            FadeOut(lc),
+        )
+
+        self.play(
+            FadeIn(good_for[0][99:]),
+            FadeIn(quantum),
+        )
+
+        title = ImageMobject("assets/handbook.png").set_width(self.camera.frame.get_width() * 0.8)
+        title.next_to(self.camera.frame, RIGHT, buff=1)
+
+        for m in self.mobjects:
+            self.remove(m)
+        self.add(p)
+        self.add(good_for)
+        self.add(idkman)
+        self.add(quantum)
+
+        self.play(
+            AnimationGroup(
+                AnimationGroup(
+                    self.camera.frame.animate.move_to(title),
+                ),
+                FadeIn(title),
+                lag_ratio=0.75,
+            )
+        )
+
+        self.play(
+            FadeOut(title)
+        )
 
 
+
+class TP(ThreeDScene):
+    def construct(self):
+        resolution_fa = 24
+        phi = 75 * DEGREES
+        theta = 100 * DEGREES
+        self.set_camera_orientation(phi=phi, theta=theta)
+
+        rate = 0.2
+
+        self.begin_ambient_camera_rotation(rate=rate)
+
+        scale = 1/750
+
+        f = lambda x, y: [x, y, (x**6 + 3*x**5 + 3*x**4*y + 3*x**4 + 6*x**3*y + x**3 + 3*x**2*y**2 + 3*x**2*y + 3*x*y**2 + y**3) * scale]
+
+        surface = Surface(
+            f,
+            resolution=(resolution_fa, resolution_fa),
+            v_range=[-3, +3],
+            u_range=[-3, +3]
+        )
+
+        surface.scale(1.2, about_point=ORIGIN)
+        surface.set_style(fill_opacity=1, stroke_color=WHITE)
+        surface.set_fill_by_checkerboard(DARK_GRAY, DARKER_GRAY, opacity=1)
+        axes = ThreeDAxes()
+
+        spanning = f(1, 1)[-1] / scale
+        orient = f(2, 0)[-1] / scale
+
+        sw = 3
+
+        a = Dot3D(point=axes.coords_to_point(1, 1, f(1, 1)[-1]), color=RED, stroke_width=sw, stroke_color=BLACK).scale(3)
+        b = Dot3D(point=axes.coords_to_point(2, 0, f(2, 0)[-1]), color=BLUE, stroke_width=sw, stroke_color=BLACK).scale(3)
+
+        self.add(axes)
+        self.add(surface)
+
+        st = Tex(f"\\textbf{{{int(spanning)}}}", color=RED, stroke_width=sw, stroke_color=BLACK)\
+                .rotate(phi, axis=RIGHT)\
+                .rotate(theta + PI / 2, axis=OUT)\
+                .move_to(a).shift(OUT).scale(1.5)
+        st.add_updater(lambda x, dt: x.rotate(rate * dt, axis=OUT))
+        st.set_opacity(0)
+
+        ori = Tex(f"\\textbf{{{int(orient)}}}", color=BLUE, stroke_width=sw, stroke_color=BLACK)\
+                .rotate(phi, axis=RIGHT)\
+                .rotate(theta + PI / 2, axis=OUT)\
+                .move_to(b).shift(OUT).scale(1.5)
+        ori.add_updater(lambda x, dt: x.rotate(rate * dt, axis=OUT))
+        ori.set_opacity(0)
+
+        self.add(st)
+        self.add(ori)
+
+        self.wait()
+        self.play(
+            FadeIn(a),
+            st.animate.set_opacity(1)
+        )
+        self.wait()
+        self.play(
+            FadeIn(b),
+            ori.animate.set_opacity(1)
+        )
+        self.wait()
